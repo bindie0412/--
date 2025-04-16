d87652725dcdff3b87b64f40c2745804600d2c7b
bindie0412 <0412bflag@gmail.com>
2025-04-16 14:46:44 +0900

    더더

--- hy8.py
+++ hy8.py
@@ -1,9 +1,4 @@
-print("2*1=2")
-print("2*2=4")
-print("2*3=6")
-print("2*4=8")
-print("2*5=10")
-print("2*6=12")
-print("2*7=14")
-print("2*8=16")
-print("2*9=18")
+i = 1
+for i in range(1, 10):
+    print("2*" + str(i) + " = " + str(2 * i))
+    i += 1
